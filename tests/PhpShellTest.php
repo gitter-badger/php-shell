e4734b849ef38afa1d39f2b3a83c3bac2af94949
bpteam <bpteam22@gmail.com>
2015-07-01 20:34:19 +0300

    empty value bug fix

---
+++ tests/PhpShellTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ec
+ * Date: 30.06.15
+ * Time: 22:27
+ * Project: php-shell
+ * @author: Evgeny Pynykh bpteam22@gmail.com
+ */
+
+namespace bpteam\PhpShell;
+
+
+class PhpShellTest {
+
+}
